590d09377a635949e2d9939dd7b232ab0c34e1b0
kaesik <108717713+kaesik@users.noreply.github.com>
2023-09-21 17:49:44 +0200

    organise templates

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('sign-in/', views.sign_in, name='sign-in'),
+
+    path('user-profile/', views.user_profile, name='user-profile'),
+    path('user-profile-settings/', views.user_profile_settings, name='user-profile-settings'),
+    path('user-account-settings/', views.user_account_settings, name='user-account-settings'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,4 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_page(request):
-    return render(request, 'main/index.html')
+    title = 'HammerSite'
+    context = {'title': title}
+    return render(request, 'main/home.html', context)
+
+def sign_in(request):
+    context = {}
+    return render(request, 'main/sign-in.html', context)
+
+def user_profile(request):
+    title = 'User Profile'
+    context = {'title': title}
+    return render(request, 'main/user-profile.html', context)
+
+def user_profile_settings(request):
+    title = 'User Profile Settings'
+    context = {'title': title}
+    return render(request, 'main/user-profile-settings.html', context)
+
+def user_account_settings(request):
+    title = 'User Account Settings'
+    context = {'title': title}
+    return render(request, 'main/user-account-settings.html', context)
